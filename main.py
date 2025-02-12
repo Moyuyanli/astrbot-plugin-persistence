70faf64fe2efa41ceb522536d0409eb3093644b5
Moyuyanli <572490972@qq.com>
2025-02-12 14:57:32 +0800

    新建

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
-from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+from astrbot.api.event import filter, AstrMessageEvent
+from astrbot.api.event.filter import event_message_type
+from astrbot.api.star import Context, Star, register
+from astrbot.core.star.filter.event_message_type import EventMessageType
 
-@register("helloworld", "Your Name", "一个简单的 Hello World 插件", "1.0.0", "repo url")
+
+@register("astrbot-persistence", "moyuyanli", "一个对于消息进行持久化的插件", "1.0.0",
+          "https://github.com/Moyuyanli/astrbot-plugin-persistence")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
-    
+
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
     @filter.command("helloworld")
     async def helloworld(self, event: AstrMessageEvent):
-        '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+        '''这是一个 hello world 指令'''  # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
         user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
+        message_str = event.message_str  # 用户发的纯文本消息字符串
+        message_chain = event.get_messages()  # 用户所发的消息的消息链 # from astrbot.api.message_components import *
         logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!")  # 发送一条纯文本消息
+
+    @event_message_type(EventMessageType.ALL)
+    async def on_private_message(self, event: AstrMessageEvent):
+        '''测试消息'''
+        yield event.plain_result("收到了一条私聊消息。")
